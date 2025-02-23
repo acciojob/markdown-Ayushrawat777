0c04b2d550b77c434dacf291992b69e67db3f92f
AYUSH RAWAT <106034287+Ayushrawat777@users.noreply.github.com>
2025-02-23 22:37:45 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,15 @@
-import React, { useState, useEffect } from "react";
+import React from 'react'
 import "../styles/App.css";
-import MarkdownEditor from "./MarkdownEditor";
+import MarkdownEditor from './MarkdownEditor';
 
 const App = () => {
-  const [markdown, setMarkdown] = useState("");
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    setLoading(true);
-    const timeout = setTimeout(() => setLoading(false), 300); // Simulate a delay
-    return () => clearTimeout(timeout);
-  }, [markdown]);
-
+    
   return (
-    <div>
-      <MarkdownEditor markdown={markdown} setMarkdown={setMarkdown} loading={loading}/>
+    <div className='app'>
+        <h1>Markdown Editor</h1>
+        <MarkdownEditor />
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
